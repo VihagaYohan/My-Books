51a690d6bba5fd9e16e75706a51dc091ed8989ef
VihagaYohan <vihagayohan94@gmail.com>
2021-06-10 01:59:21 +0530

    Added delete publisher endpoint

--- my-book/Data/Services/PublisherServices.cs
+++ my-book/Data/Services/PublisherServices.cs
@@ -42,5 +42,15 @@ namespace my_book.Data.Services
 			_context.Publishers.Add(_publisher);
 			_context.SaveChanges();
 		}
+
+		public void DeletePublisherById(int id)
+		{
+			var _publisher = _context.Publishers.FirstOrDefault(n => n.Id == id);
+			if (_publisher != null) 
+			{
+				_context.Publishers.Remove(_publisher);
+				_context.SaveChanges();
+			}
+		}
 	}
 }
